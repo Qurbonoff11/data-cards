190d31a0f608b5963846fa7fd40d4bd5d700b66c
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-07-18 01:17:57 +0500

    chala ish

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,15 @@
 import { useState } from "react";
+import { moviesData } from "./data";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { MdModeEdit } from "react-icons/md";
 
-const moviesData = [
-  {
-    id: 1,
-    image:
-      "https://images.uzmovi.tv/2025-07-17/1467a1a30198f4e5adc98d71da39cd96.jpg",
-    title: "M3GAN 2",
-    year: 2025,
-    genre: "Horror",
-    country: "USA",
-  },
-  {
-    id: 2,
-    image:
-      "https://images.uzmovi.tv/2022-11-27/51e50b24fc42cc262b84ad44ee41a128.jpg",
-    title: "Saylent hill ",
-    year: 2022,
-    genre: "Horror",
-    country: "USA",
-  },
-  {
-    id: 3,
-    image:
-      "https://images.uzmovi.tv/2022-07-08/d306460829b75839d1bfad3089724da6.jpg",
-    title: "Solum",
-    year: 2022,
-    genre: "Action",
-    country: "USA",
-  },
-  {
-    id: 4,
-    image:
-      "https://images.uzmovi.tv/2022-10-19/9a230ad9933dc6ae268dbdf8d5518db9.jpg",
-    title: "Kelajakdagi tuzoq",
-    year: 2022,
-    genre: "Fantastik",
-    country: "USA",
-  },
-  {
-    id: 5,
-    image:
-      "https://images.uzmovi.tv/2025-04-05/a8c46babdd75a0d659dcf2a4f0f0ccbc.jpg",
-    title: "Amerikalik qaroqchilar",
-    year: 2023,
-    genre: "Criminal",
-    country: "USA",
-  },
-  {
-    id: 6,
-    image:
-      "https://images.uzmovi.tv/2022-03-31/5c508a9832475a1c04a753be938b6ef4.jpg",
-    title: "Oy ritsari",
-    year: 2022,
-    genre: "Action",
-    country: "USA",
-  },
-];
-
 const App = () => {
   const [movies, setMovies] = useState(moviesData);
+
+  const handleCardDelete = (deleteID) => {
+    const filtered = movies.filter((movie) => deleteID !== movie.id);
+    setMovies(filtered);
+  };
 
   return (
     <section className="section">
@@ -68,21 +17,23 @@ const App = () => {
         <h1 className="title">Cards list</h1>
         <div className="cards-list">
           {movies.map((movie) => {
-            const { id, image, title, year, genre, country } = movie;
+            const { id, title, year, genre, country } = movie;
             return (
               <div className="card" key={id}>
-                <div className="card-image">
-                  <img src={image} alt={title} />
-                </div>
                 <div className="card-info">
                   <div className="info">
-                    <h2 className="movie-title">Nomi: <span>{title}</span></h2>
-                    <p className="movie-year">Yili: {year}</p>
-                    <p className="movie-genre">Janri: {genre}</p>
-                    <p className="movie-country">Davlat: {country}</p>
+                    <h2>
+                      Nomi: <span>{title}</span>
+                    </h2>
+                    <p>Yili: {year}</p>
+                    <p>Janri: {genre}</p>
+                    <p>Davlat: {country}</p>
                   </div>
                   <div className="btns">
-                    <button className="delete-btn">
+                    <button
+                      onClick={() => handleCardDelete(id)}
+                      className="delete-btn"
+                    >
                       <FaRegTrashAlt />
                     </button>
                     <button className="edit-btn">
@@ -93,6 +44,15 @@ const App = () => {
               </div>
             );
           })}
+        </div>
+        <div className="modal-window">
+          <div>
+            <input type="text" className="movie-name" placeholder="Movie mane..." />
+            <input type="text" className="movie-year" placeholder="Year..." />
+            <input type="text" className="movie-genre" placeholder="Genre..." />
+            <input type="text" className="movie-country" placeholder="Country..." />
+          </div>
+          <button className="edit-btn">Edit</button>
         </div>
       </div>
     </section>
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const moviesData = [
+  {
+    id: 1,
+    title: "M3GAN 2",
+    year: 2025,
+    genre: "Horror",
+    country: "USA",
+  },
+  {
+    id: 2,
+    title: "Saylent hill ",
+    year: 2022,
+    genre: "Horror",
+    country: "USA",
+  },
+  {
+    id: 3,
+    title: "Solum",
+    year: 2022,
+    genre: "Action",
+    country: "USA",
+  },
+  {
+    id: 4,
+    title: "Kelajakdagi tuzoq",
+    year: 2022,
+    genre: "Fantastik",
+    country: "USA",
+  },
+  {
+    id: 5,
+    title: "Amerikalik qaroqchilar",
+    year: 2023,
+    genre: "Criminal",
+    country: "USA",
+  },
+  {
+    id: 6,
+    title: "Oy ritsari",
+    year: 2022,
+    genre: "Action",
+    country: "USA",
+  },
+];
